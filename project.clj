7e8f9e3d0246c79075ea3b96ed70dc27abf4e965
Bradley Losavio <171940879+blosavio@users.noreply.github.com>
2024-11-27 15:00:45 +0800

    1. removed project-specific changelog generator namespace 2. delegate changelog generation to Chlog dev dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.sagevisuals/fn-in "3"
+(defproject com.sagevisuals/fn-in "4-SNAPSHOT0"
   :description "A Clojure library for manipulating heterogeneous, arbitrarily-nested data structures."
   :url "https://blosavio.github.io/fn-in"
   :license {:name "MIT License"
@@ -10,7 +10,8 @@
   :plugins []
   :profiles {:dev {:dependencies [[hiccup "2.0.0-RC3"]
                                   [zprint "1.2.9"]
-                                  [com.sagevisuals/readmoi "0"]]
+                                  [com.sagevisuals/chlog "0-SNAPSHOT2"]
+                                  [com.sagevisuals/readmoi "2-SNAPSHOT1"]]
                    :plugins [[dev.weavejester/lein-cljfmt "0.12.0"]
                              [lein-codox "0.10.8"]]}
              :repl {}}
@@ -21,5 +22,5 @@
           :source-uri "https://github.com/blosavio/fn-in/blob/main/{filepath}#L{line}"
           :html {:namespace-list :flat
                  :transforms [[:div.sidebar.primary] [:append [:ul.index-link [:li.depth-1 [:a {:href "https://github.com/blosavio/fn-in"} "Project Home"]]]]]}
-          :project {:name "fn-in" :version "version 3"}}
+          :project {:name "fn-in" :version "version 4-SNAPSHOT0"}}
   :scm {:name "git" :url "https://github.com/blosavio/fn-in"})
